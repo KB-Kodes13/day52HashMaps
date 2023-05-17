0b9427260a9d8167b74b14b2cc4825cab7a5bc5b
Kelan Blash <kjblash13@gmail.com>
2023-05-16 20:08:33 -0400

    print another hash map commit

---
+++ src/printmeanotherhashmap/Main.java
@@ -0,0 +1,18 @@
+package printmeanotherhashmap;
+import java.util.HashMap;
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Book> hashmap = new HashMap<>();
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "..."));
+
+
+        Program.printValues(hashmap);
+        System.out.println("---");
+
+
+        Program.printValueIfNameContains(hashmap, "prejud");
+    }
+}
